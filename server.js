be31ea7d3b6e7222716f88efa40c1eabe023e946
Rob Cameron <rwcameron@gmail.com>
2012-02-27 08:40:12 +0000

    "added name"

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ io.sockets.on('connection', function(socket) {
     });
 	});
   socket.on('setname', function(data){
-     socket.set('name', escape(data.name), function () {
+     socket.set('name', escape(data), function () {
       socket.emit('ready');
     });
   });
